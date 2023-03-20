6bf94c89c19365b59c4fb029467483fb0a7c6b38
Robin Adams <rob.thomas.adams@gmail.com>
2023-03-20 16:53:52 -0700

    asdf

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+# https://realpython.com/python-gui-tkinter/#building-your-first-python-gui-application-with-tkinter
+# https://tkdocs.com/tutorial/grid.html
 import tkinter as tk
 
 window = tk.Tk()
